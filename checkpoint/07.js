4e36e06a0384f10bb8980a76bb5d7bfb42290e43
mveraalbarenque <don.marcos.vera@gmail.com>
2023-02-04 13:10:59 -0300

    M1 07 PASS

--- checkpoint/07.js
+++ checkpoint/07.js
@@ -1,8 +1,8 @@
-const { LinkedList } = require('../DS');
+const { LinkedList } = require("../DS");
 // ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️
 //
 //
-// 7️⃣ ***** EJERCICIO 7 ***** - LinkedList.migrarStock() 7️⃣ 
+// 7️⃣ ***** EJERCICIO 7 ***** - LinkedList.migrarStock() 7️⃣
 //  La tienda de ropa necesita migrar sus datos de stock de una lista a un objeto.
 //  Para esto deberás agregar al prototype de LinkedList el método 📦 migrarStock() 📦, el cual deberá retornar todos los valores almacenados
 //  de la lista en un nuevo objeto con formato nombre:stock.
@@ -10,10 +10,20 @@ const { LinkedList } = require('../DS');
 // EJEMPLOS:
 //  Dada la siguiente lista:
 //
-// ({nombre: 'Camisetas'}, stock: 22) ➡ ({nombre:'Pantalones', stock: 12}) ➡ ({nombre:'Zapatos', stock: 45}) ➡ ({nombre: 'Bufandas', stock: 12}) ➡ ({nombre: 'Sacos', stock: 4})
+// ({nombre: 'Camisetas'}, stock: 22) ➡
+// ({nombre:'Pantalones', stock: 12}) ➡
+// ({nombre:'Zapatos', stock: 45}) ➡
+// ({nombre: 'Bufandas', stock: 12}) ➡
+// ({nombre: 'Sacos', stock: 4})
 //
 // lista.migrarStock() devuelve ➡
-// {Camisetas: 22, Pantalones: 12, Zapatos: 45, Bufandas: 12, Sacos: 4}
+// {
+//    Camisetas: 22,
+//    Pantalones: 12,
+//    Zapatos: 45,
+//    Bufandas: 12,
+//    Sacos: 4
+// }
 //
 //
 // REQUISITOS:
@@ -22,9 +32,21 @@ const { LinkedList } = require('../DS');
 
 LinkedList.prototype.migrarStock = function () {
   // Tu código aquí:
-}
+  let respuesta = {};
+  if (!this.cabecera()) return "No hay stock para migrar";
+  let actual = this.cabecera();
+  while (actual) {
+    respuesta[actual.value.nombre] = actual.value.stock;
+    actual = actual.next;
+  }
+  return respuesta;
+};
+LinkedList.prototype.cabecera = function () {
+  if (!this.head) return null;
+  return this.head;
+};
 
 // ⚠️ NO MODIFICAR NADA POR DEBAJO DE ESTA LÍNEA ⚠️
 module.exports = {
-  LinkedList
+  LinkedList,
 };
